1a66a3d529589f279c3e0563c44fda8f42d94457
shaik-213581 <mohammadazeem034@gmail.com>
2022-07-23 00:13:56 +0530

    Adding MVC Project

---
+++ MVCFirstProject/MVCFirstProject/Controllers/EmployeeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCFirstProject.Models
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ActionResult EmployeeInfo()
+        {
+            Employee employee = new Employee()
+            {
+                EmployeeId = 1001,
+                EmployeeName = "Azeem",
+                EmployeeLocation = "Hyderabad"
+            };
+
+            return View(employee);
+        }
+    }
+}
